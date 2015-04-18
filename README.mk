40d27f5162530c88d565f5273b8e35d89502cb33
natxo cabré <natxo.cabre@gmail.com>
2015-04-18 22:57:13 +0200

    example from persistent book

---
+++ README.mk
@@ -0,0 +1,9 @@
+# postgresql-simple examples
+Some examples and links about Haskell persistent library.
+
+## Usage
+nix-build
+result/bin/haskell-persistent-examples
+
+## Links
+- http://www.yesodweb.com/book/persistent
